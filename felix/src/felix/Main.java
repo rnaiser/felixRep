3dfc16e6e4413b21a0fb950fe5ba173c4d09c0f4
Carl <Carl@LAPTOP-VTSB0P53.localdomain>
2022-05-25 18:56:12 +0200

    lol loll lol

---
+++ felix/src/felix/Main.java
@@ -0,0 +1,12 @@
+package felix;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Felix der Glückliche");
+
+	}
+
+}
